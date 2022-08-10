f100bd53faab81bc9999202b0c0e7ef8b05a8b24
NataliaBebebino <nbebebino@gmail.com>
2022-08-10 14:52:27 -0400

    Create MainNavigation component

---
+++ frontend/src/components/MainNavigation.js
@@ -0,0 +1,23 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const MainNavigation = () => {
+  return (
+    <Navbar bg="dark" expand="lg" fixed="top" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">TMDB</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Movies</Nav.Link>
+            <Nav.Link href="#link">TV shows</Nav.Link>
+            <Nav.Link href="#link">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default MainNavigation;
