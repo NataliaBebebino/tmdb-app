c13873955d5edbb4700f7af0412535c92c3096db
NataliaBebebino <nbebebino@gmail.com>
2022-08-10 21:12:02 -0400

    Create Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import MainNavigation from "./components/MainNavigation";
 import SearchBar from "./components/SearchBar";
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <MainNavigation />
       <SearchBar />
+      <Footer />
     </div>
   );
 }
