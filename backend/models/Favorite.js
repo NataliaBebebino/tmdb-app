c8dfbd1ed7764f9bb42c289ef0004e7c2af9045c
NataliaBebebino <nbebebino@gmail.com>
2022-08-16 16:57:36 -0400

    Add combination of unique in Favorites model

--- backend/models/Favorite.js
+++ backend/models/Favorite.js
@@ -1,16 +1,20 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../database/database");
 
-const Favorite = sequelize.define("favorites", {
-  mediaId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
+const Favorite = sequelize.define(
+  "favorites",
+  {
+    mediaId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
 
-  type: {
-    type: DataTypes.ENUM("movie", "tv"),
-    allowNull: false,
+    type: {
+      type: DataTypes.ENUM("movie", "tv"),
+      allowNull: false,
+    },
   },
-});
+  { indexes: [{ unique: true, fields: ["mediaId", "type", "userId"] }] }
+);
 
 module.exports = Favorite;
