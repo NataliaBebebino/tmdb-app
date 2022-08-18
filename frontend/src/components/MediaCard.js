2b6f4f09a9243c463dc3a8758db319471b09aa65
NataliaBebebino <nbebebino@gmail.com>
2022-08-18 10:26:00 -0400

    Add not-found-img-template for not found images

--- frontend/src/components/MediaCard.js
+++ frontend/src/components/MediaCard.js
@@ -1,32 +1,33 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
+import posterNotAvailable from '../assets/posterNotAvailable.png'
 
 
 const MediaCard = (props) => {
 
   return (
-    <Link
+      <Card className="text-white bg-dark" style={{ width: "13rem" }}>
+            <Link
       to={`${props.mediaType === "movie" ? "/movies/" : "/tv-shows/"}${
         props.id
       }`}
     >
-      <Card style={{ width: "12rem" }}>
         <Card.Img
-          style={{ height: "18rem", width: "12rem" }}
+          style={{ height: "18rem", width: "100%" }}
           variant="top"
-          src={`http://image.tmdb.org/t/p/w342${props.image}`}
+          src={ props.image ? `http://image.tmdb.org/t/p/w342${props.image}` : posterNotAvailable}
         />
+        </Link>
         <Card.Body>
-          {/* <Card.Title>{props.title}</Card.Title> */}
           <div className="container">
             <div className="col-md-12 text-center">
-              <Card.Subtitle className="mb-2 text-muted">{`Ranking: ${props.average} ⭐`}</Card.Subtitle>
+              <Card.Subtitle className="mb-2 text-truncate">{props.title}</Card.Subtitle>
+              <Card.Subtitle>{`${props.average} ⭐`}</Card.Subtitle>
             </div>
           </div>
         </Card.Body>
       </Card>
-    </Link>
   );
 };
 
