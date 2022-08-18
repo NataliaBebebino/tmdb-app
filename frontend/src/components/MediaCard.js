2c18491a0260164f9b84cd2b8c89aaed7bcf0d07
NataliaBebebino <nbebebino@gmail.com>
2022-08-18 14:37:53 -0400

    Add prettier format

--- frontend/src/components/MediaCard.js
+++ frontend/src/components/MediaCard.js
@@ -1,33 +1,37 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
-import posterNotAvailable from '../assets/posterNotAvailable.png'
-
+import posterNotAvailable from "../assets/posterNotAvailable.png";
 
 const MediaCard = (props) => {
-
   return (
-      <Card className="text-white bg-dark" style={{ width: "13rem" }}>
-            <Link
-      to={`${props.mediaType === "movie" ? "/movies/" : "/tv-shows/"}${
-        props.id
-      }`}
-    >
+    <Card className="text-white bg-dark" style={{ width: "13rem" }}>
+      <Link
+        to={`${props.mediaType === "movie" ? "/movies/" : "/tv-shows/"}${
+          props.id
+        }`}
+      >
         <Card.Img
           style={{ height: "18rem", width: "100%" }}
           variant="top"
-          src={ props.image ? `http://image.tmdb.org/t/p/w342${props.image}` : posterNotAvailable}
+          src={
+            props.image
+              ? `http://image.tmdb.org/t/p/w342${props.image}`
+              : posterNotAvailable
+          }
         />
-        </Link>
-        <Card.Body>
-          <div className="container">
-            <div className="col-md-12 text-center">
-              <Card.Subtitle className="mb-2 text-truncate">{props.title}</Card.Subtitle>
-              <Card.Subtitle>{`${props.average} ⭐`}</Card.Subtitle>
-            </div>
+      </Link>
+      <Card.Body>
+        <div className="container">
+          <div className="col-md-12 text-center">
+            <Card.Subtitle className="mb-2 text-truncate">
+              {props.title}
+            </Card.Subtitle>
+            <Card.Subtitle>{`${props.average} ⭐`}</Card.Subtitle>
           </div>
-        </Card.Body>
-      </Card>
+        </div>
+      </Card.Body>
+    </Card>
   );
 };
 
