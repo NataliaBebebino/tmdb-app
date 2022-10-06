0c6cb0f6ddb496822e388e4f3b9986e4028b6a12
NataliaBebebino <nbebebino@gmail.com>
2022-10-05 21:26:36 -0400

    add allowed methods and headers to cors

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,8 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
+    methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
